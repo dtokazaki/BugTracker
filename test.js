af1656d55d5eafb4d6912c2fce4c524785d1bc7f
dtokazaki <daniel.t.okazaki@gmail.com>
2018-10-29 22:24:21 -0700

    deosnt really work

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
-const userAction = async () => {
-	const response = await fetch('https://052stnmylg.execute-api.us-west-1.amazonaws.com/firstTes', {
-		method: 'GET',
-		body: id=ea4cec393c2c4694a176930ee8c5cc11, // string or object
-		headers:{
-			'Content-Type': 'application/json'
-			'x-api-key':IRhxCk4NmV4mpQHK75NsL4GEqSocd4X56fFgX7BS
-			}
-	});
-	const myJson = await response.json(); //extract JSON from the http response
-		
-}
+var url = 'https://052stnmylg.execute-api.us-west-1.amazonaws.com/firstTest';
+var data = {id: 'ea4cec393c2c4694a176930ee8c5cc11'};
+
+fetch(url, {
+    headers:{
+        'Content-Type': 'application/json',
+        'x-api-key': 'IRhxCk4NmV4mpQHK75NsL4GEqSocd4X56fFgX7BS'
+    },
+    'mode':'no-cors'
+
+}).then(res => res.json())
+.then(response => console.log('Success:', JSON.stringify(response)))
+.catch(error => console.error('Error:', error));
+
+
