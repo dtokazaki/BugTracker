372e6464faf4dc034fd8725ae8d1c6fc46740d74
dtokazaki <daniel.t.okazaki@gmail.com>
2018-10-30 09:41:27 -0700

    trying ajaji

--- test.js
+++ test.js
@@ -1,15 +1,11 @@
 var url = 'https://052stnmylg.execute-api.us-west-1.amazonaws.com/firstTest';
 var data = {id: 'ea4cec393c2c4694a176930ee8c5cc11'};
 
-fetch(url, {
-    headers:{
-        'Content-Type': 'application/json',
-        'x-api-key': 'IRhxCk4NmV4mpQHK75NsL4GEqSocd4X56fFgX7BS'
-    },
-    'mode':'no-cors'
-
-}).then(res => res.json())
-.then(response => console.log('Success:', JSON.stringify(response)))
-.catch(error => console.error('Error:', error));
+.ajax({
+		url: "https://052stnmylg.execute-api.us-west-1.amazonaws.com/firstTest",
+		data: {id: data},
+		type: "GET",
+		beforeSend: function(xhr){xhr.setRequestHeader{'x-api-key', 'IRhxCk4NmV4mpQHK75NsL4GEqSocd4X56fFgX7BS');},
+		success: function() {alert('Success!" + data); }
 
 
